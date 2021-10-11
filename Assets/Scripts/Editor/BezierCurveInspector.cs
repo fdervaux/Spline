290076e49b04cb9bef1229c74d16a15aa020f925
fdervaux <francois.dervaux@gmail.com>
2021-10-11 11:59:52 +0200

    update

--- Assets/Scripts/Editor/BezierCurveInspector.cs
+++ Assets/Scripts/Editor/BezierCurveInspector.cs
@@ -1,14 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
-[CustomEditor(typeof(BezierCurve))]
+[CustomEditor(typeof (BezierCurve))]
 public class BezierCurveInspector : Editor
 {
     public const int segmentNumber = 100;
-
-
 
     public override void OnInspectorGUI()
     {
@@ -23,30 +21,35 @@ public class BezierCurveInspector : Editor
 
         Quaternion handleRotation = handleTransform.rotation;
 
-        if(Tools.pivotRotation == PivotRotation.Global)
+        if (Tools.pivotRotation == PivotRotation.Global)
             handleRotation = Quaternion.identity;
 
         Vector3 p0 = handleTransform.TransformPoint(bezierCurve.p0);
         Vector3 p1 = handleTransform.TransformPoint(bezierCurve.p1);
         Vector3 p2 = handleTransform.TransformPoint(bezierCurve.p2);
 
-
         Handles.color = Color.white;
 
-        Vector3 lastPoint = handleTransform.TransformPoint(bezierCurve.p0);
-        for(float i = 1 ; i< segmentNumber; i++)
+        Vector3 lastPoint = p0;
+        for (float i = 1; i < segmentNumber; i++)
         {
-            Vector3 currentPoint = handleTransform.TransformPoint(bezierCurve.computeBezierPoint(i/segmentNumber));
-            Handles.DrawLine(lastPoint,currentPoint);
+            Vector3 currentPoint =
+                handleTransform
+                    .TransformPoint(bezierCurve
+                        .computeBezierPoint(i / segmentNumber));
+            Handles.DrawLine (lastPoint, currentPoint);
+
             lastPoint = currentPoint;
         }
+
+        Handles.DrawLine (lastPoint, p2);
 
         EditorGUI.BeginChangeCheck();
         p0 = Handles.PositionHandle(p0, handleRotation);
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(bezierCurve, "Move Point");
-            EditorUtility.SetDirty(bezierCurve);
+            EditorUtility.SetDirty (bezierCurve);
             bezierCurve.p0 = handleTransform.InverseTransformPoint(p0);
         }
 
@@ -55,9 +58,8 @@ public class BezierCurveInspector : Editor
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(bezierCurve, "Move Point");
-            EditorUtility.SetDirty(bezierCurve);
+            EditorUtility.SetDirty (bezierCurve);
             bezierCurve.p1 = handleTransform.InverseTransformPoint(p1);
-            
         }
 
         EditorGUI.BeginChangeCheck();
@@ -65,21 +67,12 @@ public class BezierCurveInspector : Editor
         if (EditorGUI.EndChangeCheck())
         {
             Undo.RecordObject(bezierCurve, "Move Point");
-            EditorUtility.SetDirty(bezierCurve);
+            EditorUtility.SetDirty (bezierCurve);
             bezierCurve.p2 = handleTransform.InverseTransformPoint(p2);
         }
-
-
     }
-
 
     private void drawCurve(BezierCurve curve, Transform handleTransform)
     {
-        
     }
-
-
-
-
-}   
-
+}
