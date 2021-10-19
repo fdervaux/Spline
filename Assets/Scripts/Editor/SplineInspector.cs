a75af78e4fbcf17748ff8f46aee1b840ffc7606a
fdervaux <francois.dervaux@gmail.com>
2021-10-19 15:22:30 +0200

    update

--- Assets/Scripts/Editor/SplineInspector.cs
+++ Assets/Scripts/Editor/SplineInspector.cs
@@ -43,95 +43,38 @@ public class SplineInspector : Editor
         Vector3 oldPosition = element.vector3Value;
 
         EditorGUI.BeginChangeCheck();
-        Vector3 newPosition = EditorGUI.Vector3Field(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), element.displayName, oldPosition);
+
+        EditorGUI.PropertyField(new Rect(rect.x, rect.y, rect.width, EditorGUIUtility.singleLineHeight), element, new GUIContent("Point " + index));
+        serializedObject.ApplyModifiedProperties();
 
         if (EditorGUI.EndChangeCheck())
         {
+            Vector3 newPosition = element.vector3Value;
             movePointWithConstraint(index, oldPosition, newPosition);
         }
     }
 
-
-    void addPointAtEnd()
-    {
-
-        if (_controlPointsProperty.arraySize > 2)
-        {
-            Vector3 Tangent =
-                _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 2).vector3Value -
-                _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 3).vector3Value;
-            _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value =
-                _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 2).vector3Value + Tangent;
-        }
-
-        _controlPointsProperty.InsertArrayElementAtIndex(_controlPointsProperty.arraySize);
-        _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value += Vector3.one;
-
-        _controlPointsProperty.InsertArrayElementAtIndex(_controlPointsProperty.arraySize);
-        _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value += Vector3.one;
-    }
-
-    void AddPointAtStart()
-    {
-        _controlPointsProperty.InsertArrayElementAtIndex(_controlPointsProperty.arraySize);
-        _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value += Vector3.one;
-
-        _controlPointsProperty.InsertArrayElementAtIndex(_controlPointsProperty.arraySize);
-        _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value += Vector3.one;
-    }
-
-
     void OnAdd(ReorderableList list)
     {
-        Debug.Log("add");
 
-        if(_controlPointsProperty.arraySize == 0)
-        {
-            AddPointAtStart();
-            return;
-        }
+        _controlPointsProperty.InsertArrayElementAtIndex(_controlPointsProperty.arraySize);
+        _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value += Vector3.one;
 
-        if(_list.index >= _controlPointsProperty.arraySize - 2)
-        {
-            addPointAtEnd();
-        }
+        _controlPointsProperty.InsertArrayElementAtIndex(_controlPointsProperty.arraySize);
+        _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value += Vector3.one;
 
-
-       
+        _controlPointsProperty.InsertArrayElementAtIndex(_controlPointsProperty.arraySize);
+        _controlPointsProperty.GetArrayElementAtIndex(_controlPointsProperty.arraySize - 1).vector3Value += Vector3.one;
     }
 
     void OnRemove(ReorderableList list)
     {
-        int indexToRemove = (_list.index + 1) / 3;
+        int indexToRemove = (_list.index) / 3;
 
-        if (indexToRemove == (_controlPointsProperty.arraySize-1) / 3)
-        {
-            Debug.Log("test");
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3);
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 - 1);
-
-            if(indexToRemove > 0)
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 - 2);
-        }
-        else if (indexToRemove == 0)
-        {
-            if (_controlPointsProperty.arraySize > 0)
-                _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 + 2);
-
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 + 1);
-
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 );
-        }
-        else
-        {
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 + 1);
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3);
-            _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 - 1);
-        }
-        
-        
+        _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 + 2);
+        _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3 + 1);
+        _controlPointsProperty.DeleteArrayElementAtIndex(indexToRemove * 3);
     }
-
 
     public void OnEnable()
     {
@@ -177,7 +120,7 @@ public class SplineInspector : Editor
         for (int i = 0; i < _controlPointsProperty.arraySize; i++)
         {
 
-            if ((i + 1) / 3 == (selectedIndex + 1) / 3)
+            if (i / 3 == selectedIndex / 3)
                 Handles.color = Color.green;
             else
                 Handles.color = Color.gray;
@@ -192,26 +135,16 @@ public class SplineInspector : Editor
         {
             List<Vector3> points = new List<Vector3>();
 
-            if ((i + 1) / 3 == (selectedIndex + 1) / 3)
+            if (i / 3 == selectedIndex / 3)
                 Handles.color = Color.green;
             else
                 Handles.color = Color.gray;
 
-            if (i > 0)
-            {
-                points.Add(handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i - 1).vector3Value));
-            }
-
             points.Add(handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i).vector3Value));
-
-            if (i < _controlPointsProperty.arraySize - 1)
-            {
-                points.Add(handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i + 1).vector3Value));
-            }
-
+            points.Add(handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i + 1).vector3Value));
+            points.Add(handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i + 2).vector3Value));
 
             Handles.DrawAAPolyLine(points.ToArray());
-
         }
     }
 
@@ -220,13 +153,13 @@ public class SplineInspector : Editor
     private void drawSpline()
     {
         int nbCurves = _controlPointsProperty.arraySize / 3;
-        for (int i = 0; i < nbCurves; i++)
+        for (int i = 0; i < nbCurves-1; i++)
         {
             Handles.DrawBezier(
-                handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i * 3).vector3Value),
-                handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i * 3 + 3).vector3Value),
                 handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i * 3 + 1).vector3Value),
+                handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i * 3 + 4).vector3Value),
                 handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i * 3 + 2).vector3Value),
+                handleTransform.TransformPoint(_controlPointsProperty.GetArrayElementAtIndex(i * 3 + 3).vector3Value),
                 Color.white,
                 null,
                 2
@@ -266,42 +199,26 @@ public class SplineInspector : Editor
         Undo.RecordObject(spline, "Move Point");
         EditorUtility.SetDirty(spline);
 
-        if (index % 3 == 0)
-        {
-            Vector3 displacment = newPosition - oldPosition;
-
-            if (index > 0)
-            {
-                _controlPointsProperty.GetArrayElementAtIndex(index - 1).vector3Value += displacment;
-            }
-            _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value = newPosition;
-            if (index < _controlPointsProperty.arraySize - 1)
-            {
-                _controlPointsProperty.GetArrayElementAtIndex(index + 1).vector3Value += displacment;
-            }
-        }
-
-
         if (index % 3 == 1)
         {
+            Vector3 displacment = newPosition - oldPosition;
+            _controlPointsProperty.GetArrayElementAtIndex(index - 1).vector3Value += displacment;
             _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value = newPosition;
-            if (index > 1)
-            {
-                Vector3 Tangent = _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value - _controlPointsProperty.GetArrayElementAtIndex(index - 1).vector3Value;
-                _controlPointsProperty.GetArrayElementAtIndex(index - 2).vector3Value = _controlPointsProperty.GetArrayElementAtIndex(index - 1).vector3Value - Tangent;
-            }
-
+            _controlPointsProperty.GetArrayElementAtIndex(index + 1).vector3Value += displacment;
         }
 
         if (index % 3 == 2)
         {
             _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value = newPosition;
-            if (index < _controlPointsProperty.arraySize - 2)
-            {
-                Vector3 Tangent = _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value - _controlPointsProperty.GetArrayElementAtIndex(index + 1).vector3Value;
-                _controlPointsProperty.GetArrayElementAtIndex(index + 2).vector3Value = _controlPointsProperty.GetArrayElementAtIndex(index + 1).vector3Value - Tangent;
-            }
+            Vector3 Tangent = _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value - _controlPointsProperty.GetArrayElementAtIndex(index - 1).vector3Value;
+            _controlPointsProperty.GetArrayElementAtIndex(index - 2).vector3Value = _controlPointsProperty.GetArrayElementAtIndex(index - 1).vector3Value - Tangent;
+        }
 
+        if (index % 3 == 0)
+        {
+            _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value = newPosition;
+            Vector3 Tangent = _controlPointsProperty.GetArrayElementAtIndex(index).vector3Value - _controlPointsProperty.GetArrayElementAtIndex(index + 1).vector3Value;
+            _controlPointsProperty.GetArrayElementAtIndex(index + 2).vector3Value = _controlPointsProperty.GetArrayElementAtIndex(index + 1).vector3Value - Tangent;
         }
     }
 }
--- Assets/Scripts/Runtime/Spline.cs
+++ Assets/Scripts/Runtime/Spline.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Spline : MonoBehaviour
 {
     [SerializeField, HideInInspector] private Vector3[] controlPoints = new Vector3[0]; 
